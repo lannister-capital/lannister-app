04736d9d67ad18add3a8430c792e6b1846edcf17
Tiago Alves <alvesjtiago@gmail.com>
2019-06-14 15:46:12 +0100

    Start testing blockstack login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import * as blockstack from 'blockstack'
 
 import MainContainer from './components/MainContainer'
 import Sidebar from './components/Sidebar'
@@ -22,6 +23,12 @@ const LargeColumn = styled(Column)`
 `
 
 function App() {
+  const { origin } = window.location
+  blockstack.redirectToSignIn(
+    origin,
+    'https://lannister.capital/manifest.json',
+    ['store_write']
+  )
   return (
     <Router>
       <div className="App">
