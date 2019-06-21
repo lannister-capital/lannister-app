7a98f008919c03b4697117c0cf5dec08f4b70e45
Tiago Alves <alvesjtiago@gmail.com>
2019-06-21 23:54:39 +0100

    Add total value and pie chart to holding

--- src/views/Holding.tsx
+++ src/views/Holding.tsx
@@ -1,17 +1,47 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
+import accounting from 'accounting'
+import { PieChart, Pie, Cell } from 'recharts'
 import db from '../db'
 
-const Holding = props => {
+const TotalValue = styled.div`
+  color: #2a364a;
+  font-size: 28px;
+`
+
+const Holding = (props: { match: { params: { id: string } } }) => {
   const id = props.match.params.id
+
+  const holdings = db.read('holdings').value().holdings
+  const totalHoldingsValue = holdings.reduce((a, b) => a + b.value, 0)
   const holding = db
     .get('holdings')
     .find({ id: id })
+    .value()
+  const currencySymbol = db
+    .get('currencies')
+    .find({ code: holding.currency })
     .value()
 
   return (
     <div>
       <h1>{holding.name}</h1>
+      <TotalValue>
+        {accounting.formatMoney(holding.value, currencySymbol)}
+      </TotalValue>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={[holding, { value: totalHoldingsValue - holding.value }]}
+          dataKey="value"
+          cx={200}
+          cy={200}
+          innerRadius={70}
+          outerRadius={90}
+          fill="#82ca9d"
+          label>
+          <Cell fill={holding.color} />
+        </Pie>
+      </PieChart>
     </div>
   )
 }
