986839bff6e7558fffeb6eb1982bd52b6f8afdde
Clarence Chan <clarencejy1234@gmail.com>
2019-07-11 21:39:02 -0400

    Exporting data to csv

---
+++ src/utils/export.ts
@@ -0,0 +1,32 @@
+import { Parser } from 'json2csv'
+
+export const exportData = (data) => {
+    const fileName = new Date().toISOString().slice(0, 10) + '-data'
+
+    // Current list of fields we want to export
+    const fields = ['id', 'name', 'value', 'color', 'currency', 'convertedValue', 'totalValue']
+
+    // Add in the totalValue to make parsing easier in the json.
+    let totalValue = {
+        'totalValueInGlobalCurrency' : 0
+    }
+
+    // Sum the converted value
+    data.forEach((obj) => {
+        totalValue['totalValue'] += obj['convertedValue']
+    })
+    data.push(totalValue)
+
+    // Parse the data
+    const parser = new Parser({ fields })
+    let csv = parser.parse(data)
+
+    // Allow it to be downloadable
+    let link = document.createElement('a')
+    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv))
+    link.setAttribute('download', fileName)
+    link.style.visibility = 'hidden'
+    document.body.appendChild(link)
+    link.click();
+    document.body.removeChild(link)
+}
